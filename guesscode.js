23ac8b249d46c673d024da10aabf0d1a5d3e21e5
Jonathan Ligh <JonathankLigh@gmail.com>
2016-05-14 04:45:23 -0400

    GuessingGame skeleton

---
+++ guesscode.js
@@ -0,0 +1,6 @@
+function status(){
+	testSatus.style.display="block", testSatus.innerHTML="You are " + closeness()
+}
+function validGuess(){
+	guess = parseInt(document.getElementById("guess").value,10),null === numberToGuess?
+}
